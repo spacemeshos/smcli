f0223e72ced54e109bbc3d0baf05d747c7279d38
Jon Zlotnik <me@jonzlotnik.info>
2022-08-31 08:14:56 -0400

    fix typo from cobra-cli autogen

--- cmd/node_status.go
+++ cmd/node_status.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -19,7 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		fmt.Println("status called")
 	},
 }
 
