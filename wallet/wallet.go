27d2b078bcb5a8a86c55dd55c253b95e93567939
Jon Zlotnik <me@jonzlotnik.info>
2022-09-06 11:27:18 -0400

    exporting wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -3,7 +3,10 @@ package wallet
 import (
 	"crypto/rand"
 	"crypto/sha512"
+	"encoding/json"
+	"os"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/spf13/cobra"
 	"github.com/xdg-go/pbkdf2"
 )
@@ -41,7 +44,7 @@ func WithPassword(password string) WalletKeyOpt {
 		)
 	}
 }
-func (k *WalletKey) encrypt() ([]byte, error) {
+func (k *WalletKey) encrypt([]byte) []byte {
 	panic("implement me")
 }
 
@@ -50,6 +53,13 @@ type WalletOpener interface {
 }
 type WalletExporter interface {
 	Export(path string) error
+}
+
+type ExportableWallet struct {
+	// EncryptedWallet is the encrypted wallet data in base58 encoding.
+	EncryptedWallet string `json:"encrypted_wallet"`
+	// Salt is the salt used to derived the wallet's encryption key in base58 encoding.
+	Salt string `json:"salt"`
 }
 
 type WalletStore struct {
@@ -69,29 +79,40 @@ func (s *WalletStore) Open(path string, w *Wallet) error {
 }
 
 func (s *WalletStore) Export(path string, w *Wallet) error {
-	panic("implement me")
+	encWallet := s.wk.encrypt(w.ToBytes())
+	ew := &ExportableWallet{
+		Salt:            base58.Encode(s.wk.salt),
+		EncryptedWallet: base58.Encode(encWallet),
+	}
+	jsonWallet, err := json.Marshal(ew)
+	cobra.CheckErr(err)
+	cobra.CheckErr(os.WriteFile(path, jsonWallet, 0660))
+	return nil
 }
 
 // Wallet is a collection of accounts.
 type Wallet struct {
-	accounts []Account `json:"accounts"`
+	accounts []*Account
 }
 
 func NewWallet() *Wallet {
 	w := &Wallet{
-		accounts: make([]Account, 0),
+		accounts: make([]*Account, 0),
 	}
 	return w
 }
-func (w *Wallet) AddAccount(a Account) {
+func (w *Wallet) ToBytes() []byte {
+	panic("implement me")
+}
+func (w *Wallet) AddAccount(a *Account) {
 	w.accounts = append(w.accounts, a)
 }
 
 type Account struct {
-	Id           string `json:"id"`
-	FriendlyName string `json:"friendly_name"`
-	PrivateKey   string `json:"private_key"`
-	PublicKey    string `json:"public_key"`
+	Id           string
+	FriendlyName string
+	PrivateKey   string
+	PublicKey    string
 }
 
 func newAccount() *Account {
